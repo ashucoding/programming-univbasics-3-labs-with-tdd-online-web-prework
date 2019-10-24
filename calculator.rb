19fca42d4fc60f69870b0e4c0a8264bc6b41dd4f
Shana Wiltshire <shanawiltshire@gmail.com>
2019-10-24 01:58:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,5 @@
 first_number = 5
 second_number = 10
-  sum = first_number + second_number
+  puts first_number + second_number
+  puts first_number - second_number
+  puts first_number * second_number
